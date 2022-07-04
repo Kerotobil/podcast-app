6b2cdee62f4c7965adcf90474ca208ebf5bab1e0
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-07-04 18:11:19 +0300

    Edit next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: '/cats',
+        destination: 'https://meowfacts.herokuapp.com',
+      },
+      {
+        source: '/nox/:path*',
+        destination: 'https://nox-podcast-api.vercel.app/:path*',
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+};
