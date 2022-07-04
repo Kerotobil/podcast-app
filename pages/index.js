38f2ea0babc3439010dbcdf0a00f1664393b7238
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-07-04 18:11:05 +0300

    Delete unnecessary lines

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -8,29 +7,6 @@ export default function Home() {
         <title>Nox Podcast App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="">
-        <h1 className="">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="">
-          Get started by editing <code className="">pages/index.js</code>
-        </p>
-      </main>
-
-      <footer className="">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className="">
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
