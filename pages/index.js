324e0ba421f34790730ec59b703402c590b4a746
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-07-05 14:28:56 +0300

    Add login page router on home pahe

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/login');
+  }, []);
   return (
     <div className="bg-pink-400">
       <Head>
