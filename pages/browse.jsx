51ec06754b457e064980c52e0bc72a71b6745295
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-07-05 15:12:48 +0300

    Edit screen width

--- pages/browse.jsx
+++ pages/browse.jsx
@@ -2,8 +2,8 @@ import { BrowseScreen } from '../components/browse/screen';
 
 export default function Browse() {
   return (
-    <div className="mx-auto max-w-sm h-screen min-h-screen bg-gray-900 max-h-screen">
-      <div className="pr-4 w-5/6 mx-auto pt-12">
+    <div className="mx-auto max-w-md h-screen min-h-screen bg-gray-900 max-h-screen">
+      <div className="px-4 mx-auto pt-12">
         <BrowseScreen />
       </div>
     </div>
--- pages/login.jsx
+++ pages/login.jsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 import { LoginScreen } from '../components/login/screen';
 export default function Login() {
   return (
-    <div className="mx-auto max-w-sm h-screen relative">
-      <div className="absolute top-0 px-1 w-11/12 pr-4 pb-24 pt-12 bg-black rounded-br-xl z-10">
+    <div className="mx-auto max-w-md h-screen relative">
+      <div className="absolute top-0 px-1 w-11/12 pr-4 pb-24 pt-12 bg-gray-900 rounded-br-xl z-10">
         <LoginScreen />
       </div>
       <div>
-        <div className="fixed top-0 w-96 h-full z-0">
+        <div className="fixed top-0 max-w-md md:max-w-lg w-full h-full z-0">
           <Image src={'/Bg.png'} layout="responsive" height={16} width={9} />
         </div>
       </div>
