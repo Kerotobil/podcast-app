c59b33b51e1bea9e4ee5f4831b8187026114a1a7
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-07-05 11:14:08 +0300

    Edit axios instance for authorization

--- helpers/axios-instance.js
+++ helpers/axios-instance.js
@@ -1,6 +1,14 @@
 import Axios from 'axios';
+import store from '../store';
 
 export const axiosClient = Axios.create({
   withCredentials: true,
   baseURL: '/nox',
+});
+
+axiosClient.interceptors.request.use(function (config) {
+  const { token } = store.getState().auth;
+  config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
 });
