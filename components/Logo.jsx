fd54ba07edec8ea852b99f2094a40c0f38b7393b
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-07-05 15:13:42 +0300

    Add router on logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Logo = () => {
   return (
-    <div className="w-36 h-16 relative">
-      <Image src={'/logo.png'} layout="fill" objectFit="contain" />
-    </div>
+    <Link href={'/'}>
+      <div className="w-36 h-16 relative cursor-pointer">
+        <Image src={'/logo.png'} layout="fill" objectFit="contain" />
+      </div>
+    </Link>
   );
 };
