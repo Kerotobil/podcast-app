f950c7d286eddab160b8f94ce38b0aabbc629869
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-07-05 14:23:19 +0300

    Add podcast card

---
+++ components/browse/podcastCard.jsx
@@ -0,0 +1,63 @@
+import finalPropsSelectorFactory from 'react-redux/es/connect/selectorFactory';
+
+export const PodcastCard = (props) => {
+  return (
+    <div className="h-48 w-full py-4 px-4 flex flex-col justify-between bg-gray-800 mx-auto rounded-xl rounded-br-none">
+      <div>
+        <h1 className="text-center line-clamp-2 text-white text-2xl">{props.title}</h1>
+      </div>
+
+      <div className="flex justify-between mt-8 ">
+        <div>
+          <div className="flex items-center space-x-2 text-sm text-gray-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+              />
+            </svg>
+            <p className="">{props.likes}</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"
+              />
+            </svg>
+            <p className="text-sm text-gray-400">{props.dislikes}</p>
+          </div>
+          <p className="text-lg text-white">{props.author}</p>
+        </div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-12 w-12 bg-white fill-red-500 rounded-full "
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
